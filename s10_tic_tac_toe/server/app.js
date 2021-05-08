b94c36530cf96e8a53be00acbfbc5ccabaf44645
Leo16012000 <67458956+Leo16012000@users.noreply.github.com>
2021-05-08 10:52:08 +0700

    update win condition

--- s10_tic_tac_toe/server/app.js
+++ s10_tic_tac_toe/server/app.js
@@ -1,9 +1,19 @@
 const http = require("http");
+const { SSL_OP_NO_TLSv1_2 } = require("node:constants");
 
-const hostname = "127.0.0.1";
+const hostname = "localhost";
 const port = 3001;
 
-const winCondition = [{}];
+const winCondition = [
+  [0, 4, 8],
+  [2, 4, 6],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+];
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
