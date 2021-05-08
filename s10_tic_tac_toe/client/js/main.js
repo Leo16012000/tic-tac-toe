1b610df96eef8acd6f9cb79fa0a4bfc799380963
Hoai-Nam Truong <thnm312@gmail.com>
2021-05-08 15:21:07 +0700

    up

--- s10_tic_tac_toe/client/js/main.js
+++ s10_tic_tac_toe/client/js/main.js
@@ -113,7 +113,9 @@ const toggleCurrentTurn = () => {
   }
 }
 
+var isEnd = false;
 const handleCellClick = (e, idx) => {
+  if(isEnd) return;
   const element = e.target;
   
 
@@ -151,11 +153,15 @@ const handleCellClick = (e, idx) => {
       let text = '';
       currentTurn === TURN.CROSS ? text="O Thắng !!!" : text="X Thắng !!!";
       $('.game-replay p').html(text);
+      isEnd = true;
+      return;
     }
     else{
       if(cellValues.every(x=>x!==0)){
         $('.game-replay').css({'display': 'block'});
         $('.game-replay p').html("Hoà !!!");
+        isEnd = true;
+        return;
       }
     }
 
@@ -174,6 +180,13 @@ cellElementList.forEach((element, idx) => {
 
 $(document).ready(function(){
   // console.log(matrix)
+  inn = `
+        <tr>
+          <td>data.name</td>
+          <td>data.score</td>
+        </tr>
+      `
+  $('table tbody').append(inn);
   $('#replayGame button').on('click', function(){
     location.reload();
   });
@@ -194,7 +207,7 @@ $(document).ready(function(){
       `
     }
     
-    $('table').append(inn);
+    $('table tbody').append(inn);
   });
 
 })
